0a3318daa487f0708d2949edda24950fbd039476
Hisato Shoji <francais.harry@gmail.com>
2021-05-05 20:43:53 +0900

    Clean up unused import

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import asyncio
 import io
-import glob
 import os
-import sys
-import time
-import uuid
-from urllib.parse import urlparse
-from io import BytesIO
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
