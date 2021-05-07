39d48a43526620053da4fa48daf76b08b94a331d
Hisato Shoji <francais.harry@gmail.com>
2021-05-07 22:24:51 +0900

    Added URL format check logic

--- FaceIndentification/__init__.py
+++ FaceIndentification/__init__.py
@@ -1,24 +1,22 @@
 import logging
-
 import azure.functions as func
-
+from urllib.parse import urlparse
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    input_url = req.params.get('input_url')
-    if not input_url:
+    input_url_string = req.params.get('input_url')
+    if not input_url_string:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            input_url = req_body.get('input_url')
+            input_url_string = req_body.get('input_url')
 
-    if input_url:
-        return func.HttpResponse(f"Hello world.")
-    else:
-        return func.HttpResponse(
-             "Invalid request parameter",
-             status_code=400
-        )
+    if input_url_string:
+        input_url = urlparse(input_url_string)
+        if input_url.scheme == "https":
+            return func.HttpResponse(f"Hello world.")
+
+    return func.HttpResponse("Invalid request parameter", status_code=400)
